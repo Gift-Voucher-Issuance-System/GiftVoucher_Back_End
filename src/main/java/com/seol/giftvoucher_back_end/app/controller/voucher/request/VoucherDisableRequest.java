c4a0e12f8015e498b9633280dbc4138c5845133a
윤설이 <olerlmin1234@gmail.com>
2025-01-29 14:38:15 +0900

    [기본 구현] API 구현 - 상품권 발행, 상품권 사용, 상품권 폐기 API 구현

---
+++ src/main/java/com/seol/giftvoucher_back_end/app/controller/voucher/request/VoucherDisableRequest.java
@@ -0,0 +1,6 @@
+package com.seol.giftvoucher_back_end.app.controller.voucher.request;
+
+import com.seol.giftvoucher_back_end.common.type.VoucherAmountType;
+
+public record VoucherDisableRequest(String code) {
+}
---
+++ src/main/java/com/seol/giftvoucher_back_end/app/controller/voucher/response/VoucherPublishResponse.java
@@ -0,0 +1,4 @@
+package com.seol.giftvoucher_back_end.app.controller.voucher.response;
+
+public record VoucherPublishResponse(String code) {
+}
