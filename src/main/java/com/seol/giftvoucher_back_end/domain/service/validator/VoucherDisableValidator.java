6dbbdb1e0d0a9ea919472b1d5ed5676ea88d6b34
윤설이 <olerlmin1234@gmail.com>
2025-01-30 15:59:41 +0900

    [유효성 검사 추가] 리팩토링: 검증 로직을 직관적인 한글 명칭으로 변경

--- src/main/java/com/seol/giftvoucher_back_end/domain/service/validator/VoucherDisableValidator.java
+++ src/main/java/com/seol/giftvoucher_back_end/domain/service/validator/VoucherDisableValidator.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VoucherDisableValidator {
+
     public void validate(VoucherEntity voucherEntity, RequestContext requestContext) {
+        상품권_사용_불가_처리_권한이_있는지_확인(voucherEntity, requestContext);
+    }
+
+    private static void 상품권_사용_불가_처리_권한이_있는지_확인(VoucherEntity voucherEntity, RequestContext requestContext) {
         if (voucherEntity.publishHistory().requesterType() != requestContext.requesterType()
                 || !voucherEntity.publishHistory().requesterId().equals(requestContext.requesterId())) {
             throw new IllegalArgumentException("사용 불가 처리 권한이 없는 상품권 입니다.");
